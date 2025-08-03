9bfbd803b212c8454225c5aa0c7ef78bddb5b0fe
Pro57327 <yadavprithviraj02@gmail.com>
2025-08-03 14:29:18 +0530

    your update message

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def reset_chat():
     model.reset_history()
     return {'status': 'reset'}
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
